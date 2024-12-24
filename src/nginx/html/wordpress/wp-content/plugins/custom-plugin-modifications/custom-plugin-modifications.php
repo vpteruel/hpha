8a18999a0ec7939ac16b19d4635cb35cfd3c0e3c
Vinicius Teruel <vpteruel@gmail.com>
2024-12-24 11:10:24 -0500

    add holiday templates and remove Happy Holidays plugin

--- src/nginx/html/wordpress/wp-content/plugins/custom-plugin-modifications/custom-plugin-modifications.php
+++ src/nginx/html/wordpress/wp-content/plugins/custom-plugin-modifications/custom-plugin-modifications.php
@@ -41,69 +41,6 @@ function write_debug_log( $message ) {
     file_put_contents( CUSTOM_PLUGIN_MODIFICATIONS_LOG_FILE, $log_entry, FILE_APPEND );
 }
 
-add_action( 'login_head', 'custom_login_css' );
-
-function custom_login_css() {
-    echo '
-    <style type="text/css">
-		.login h1 a {
-			background-image: 
-				url(https://formsanyware.hpha.ca/wp-content/uploads/2024/08/robot.png),	
-				url(https://formsanyware.hpha.ca/wp-content/uploads/2024/08/logo.png);
-			background-size: 64px, contain;
-			background-position: top right, center;
-			background-repeat: no-repeat, no-repeat;
-            background-blend-mode: hue;
-			margin: 0;
-			width: 100%;
-			height: 338px;
-			pointer-events: none;
-			cursor: default;
-		}
-        
-		#loginform {
-			background-color: transparent;
-			border: 0;
-			box-shadow: none;
-		}
-        #loginform:before {
-            content: "Forms Anyware";
-            font-size: 2.48rem;
-            color: #2271b1;
-        }
-        
-        .login form { margin-top: 0; }
-		.login label { display: none; }
-		.login .wp-pwd button { display: none !important; }
-		.login .button.wp-hide-pw .dashicons { display: none; }
-		.login form .input { display: none; }
-		.login form .forgetmenot { display: none; }
-		.login #nav a, .login #backtoblog a { display: none; }
-		.login .submit { display: none; }
-
-		.login #mo_saml_button { 
-            height: auto !important;
-            margin-top: 20px;
-        }
-		.login #mo_saml_button b { display: none; }
-		.login #mo_saml_login_sso_button { 
-			font-size: 1.2rem !important;
-			margin-bottom: 0 !important; 
-		}
-
-        #login-message {
-            position: absolute;
-            top: 32px;
-        }
-    </style>';
-}
-
-add_filter('login_redirect', 'admin_default_page');
-
-function admin_default_page() {
-    return '/home';
-}
-
 /*
 Add files mime type to WordPress.
 */
